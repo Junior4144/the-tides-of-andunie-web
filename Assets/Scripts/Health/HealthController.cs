c0ed7e5167db05c6b8986a51c0e33ea4a6d9b22a
dsims2547 <simsdarryl80@gmail.com>
2025-09-28 13:09:43 -0500

    Added Health Script and Folder
    All units can use the health script to have current and max health and take damage over time. If their health reaches 0 the OnDied event is triggered. I added the script to the hero Aldarion prefab, and when he dies he stops moving and his collider is turned off.

---
+++ Assets/Scripts/Health/HealthController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthController : MonoBehaviour
+{
+    // Allows you to change private variables in Unity
+    [SerializeField]
+    private float _currentHealth = 100;
+
+    [SerializeField]
+    private float _maxHealth = 100;
+
+    [SerializeField]
+    private float _damageOverTime = 10;
+
+    [SerializeField]
+    // In seconds
+    private float _damageInterval = 1;
+
+    void Start()
+    {
+        StartCoroutine(DamageOverTime());
+    }
+
+    public float GetPercentHealth()
+    {
+        return _currentHealth / _maxHealth;
+    }
+
+    public UnityEvent OnDied;
+
+    private void TakeDamage(float amount)
+    {
+        if (_currentHealth == 0)
+        {
+            return;
+        }
+
+        _currentHealth -= amount;
+
+        if (_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
+
+        if (_currentHealth == 0)
+        {
+            OnDied.Invoke();
+        }
+    }
+
+    private void AddHealth(float amount)
+    {
+        if (_currentHealth == _maxHealth)
+        {
+            return;
+        }
+
+        _currentHealth += amount;
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+    }
+
+    // Necessary for execution across multiple frames
+    private IEnumerator DamageOverTime()
+    {
+        // So units can suffer from damage over time effects
+        while (true)
+        {
+            yield return new WaitForSeconds(_damageInterval);
+            TakeDamage(_damageOverTime);
+        }
+    }
+}
