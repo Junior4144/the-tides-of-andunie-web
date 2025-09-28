e852a952cd6d95bc417bdf8dba82403efff936a3
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-09-27 20:28:38 -0500

    adds basic Main Menu,
    To help facilitate level 0 interactions regarding hero's death.

---
+++ Assets/Scripts/Main Menu/Main Menu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene("Level 0");
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
