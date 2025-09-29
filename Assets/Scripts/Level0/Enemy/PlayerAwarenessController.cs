b60587adf70f966aab92e034893ebba36583b730
mosalah_windowsPC <mosalah3480@gmail.com>
2025-09-28 21:49:37 -0500

    Add null check for when player tag is not found

--- Assets/Scripts/Level0/Enemy/PlayerAwarenessController.cs
+++ Assets/Scripts/Level0/Enemy/PlayerAwarenessController.cs
@@ -9,15 +9,20 @@ public class PlayerAwarenessController : MonoBehaviour
 
     private void Awake()
     {
-        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
+
+        if (_playerTransform == null)
+        {
+            Debug.LogError("No game object with tag 'Player' found in the scene. Defaulting to self transform.");
+            _playerTransform = transform;
+        }
     }
 
     void Update()
-    {
-        
-    }
+    {}
 
-    public Vector2 DirectionToPlayer => (_playerTransform.position - transform.position).normalized;
-    public bool AwareOfPlayer => (_playerTransform.position - transform.position).magnitude <= _enemyAttribute.PlayerAwarenessDistance;
+    Vector2 _vectorToPlayer => _playerTransform.position - transform.position;
+    public Vector2 DirectionToPlayer => _vectorToPlayer.normalized;
+    public bool AwareOfPlayer => _vectorToPlayer.magnitude <= _enemyAttribute.PlayerAwarenessDistance;
     
 }
