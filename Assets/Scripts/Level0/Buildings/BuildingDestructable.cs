f2a8bbe3d469f992aa2c51983907f27e2943aa17
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-09-30 22:10:12 -0500

    adds destruction only occurs if in camera view

--- Assets/Scripts/Level0/Buildings/BuildingDestructable.cs
+++ Assets/Scripts/Level0/Buildings/BuildingDestructable.cs
@@ -5,6 +5,14 @@ public class BuildingDestructable : MonoBehaviour
     public GameObject explosion;
     public GameObject fire;
     public bool hasExploded = false;
+    Camera _camera;
+
+
+    private void LateUpdate()
+    {
+        _camera = Camera.main;
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,11 +25,23 @@ public class BuildingDestructable : MonoBehaviour
     {
         if (!hasExploded)
         {
-            Instantiate(explosion, transform.position, Quaternion.identity);
-            hasExploded = true;
-            Instantiate(fire, transform.position, Quaternion.identity);
-        }
+
+            Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+            if (screenPosition.x >= 0 &&
+                screenPosition.x <= _camera.pixelWidth &&
+                screenPosition.y >= 0 &&
+                screenPosition.y <= _camera.pixelHeight)
+            {
+                Instantiate(explosion, transform.position, Quaternion.identity);
+                hasExploded = true;
+                Instantiate(fire, transform.position, Quaternion.identity);
+            }
         
+
+
+        }
+
     }
     
 }
