a875db91c93787c55c48fe825e0402c97283ec85
jsnvu <jasonvu@gmail.com>
2025-10-01 12:55:57 -0500

    explosion sound

--- Assets/Scripts/Level0/Buildings/BuildingDestructable.cs
+++ Assets/Scripts/Level0/Buildings/BuildingDestructable.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class BuildingDestructable : MonoBehaviour
 {
     public GameObject explosion;
+    public AudioClip explosionSound;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("CannonBall")){
             HandleExplosion();
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 
     void HandleExplosion()
     {
-        Instantiate(explosion, transform.position, Quaternion.identity);
+        GameObject explosionInstance = Instantiate(explosion, transform.position, Quaternion.identity);
+        AudioSource audioSource = explosionInstance.GetComponent<AudioSource>();
+
+        if (audioSource != null && explosionSound != null)
+        {
+            audioSource.PlayOneShot(explosionSound, 5.0f);
+        }
     }
     
 }
