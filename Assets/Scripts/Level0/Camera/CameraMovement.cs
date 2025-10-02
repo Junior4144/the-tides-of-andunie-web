a04543742d7240e1282e5966775896183904ce72
ukaday <hayhaygrogh@gmail.com>
2025-10-02 02:00:33 -0500

    adds camera following on lvl0

--- Assets/Scripts/Level0/Camera/CameraMovement.cs
+++ Assets/Scripts/Level0/Camera/CameraMovement.cs
@@ -2,13 +2,32 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float targetSpeed = 3f;   
-    public float accelerationTime = 3f; 
+    public Transform player;
+    public float minSpeed = 3f;
+    public float maxSpeed = 5f;
+    public float accelerationTime = 3f;
     private float currentSpeed = 0f;
+
+    public Vector3 direction;
+    public float edgeDistance = 10f;
 
     void Update()
     {
+        float targetSpeed = player != null ? CalculateTargetSpeed() : minSpeed;
+        MoveCamera(targetSpeed);
+    }
+
+    private float CalculateTargetSpeed() => Mathf.Lerp(minSpeed, maxSpeed, CalculateDistanceToCameraEdge());
+
+    private float CalculateDistanceToCameraEdge()
+    {
+        float distanceInDirection = Vector3.Dot(player.position - transform.position, direction.normalized);
+        return Mathf.Clamp01(distanceInDirection / edgeDistance);
+    }
+
+    private void MoveCamera(float targetSpeed)
+    {
         currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime / accelerationTime);
-        transform.position += Vector3.left * currentSpeed * Time.deltaTime;
+        transform.position += direction.normalized * currentSpeed * Time.deltaTime;
     }
 }
