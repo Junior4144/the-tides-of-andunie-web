25102fae815ebffc84473895325c367f000277bd
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-10-01 20:55:52 -0500

    adds EndGame script

--- Assets/Scripts/Level0/Buildings/BuildingDestructionCircle.cs
+++ Assets/Scripts/Level0/Buildings/BuildingDestructionCircle.cs
@@ -15,19 +15,10 @@ public class BuildingDestructionCircle : BuildingDestructable
             {
                 Instantiate(explosion, transform.position, Quaternion.identity);
                 hasExploded = true;
-                // Center fire
                 Instantiate(fire, transform.position, Quaternion.identity);
-
-                // Fire 1 unit to the right
                 Instantiate(fire, transform.position + new Vector3(1f, 0f, 0f), Quaternion.identity);
-
-                // Fire 1 unit to the left
                 Instantiate(fire, transform.position + new Vector3(-1f, 0f, 0f), Quaternion.identity);
-
-                // Fire 1 unit up
                 Instantiate(fire, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity);
-
-                // Fire 1 unit down
                 Instantiate(fire, transform.position + new Vector3(0f, -1f, 0f), Quaternion.identity);
             }
         }
