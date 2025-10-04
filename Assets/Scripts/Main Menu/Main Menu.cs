b1507536a47a9661d94c153b8c212044a1a9021b
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-10-04 02:12:35 -0500

    cleanup

--- Assets/Scripts/Main Menu/Main Menu.cs
+++ Assets/Scripts/Main Menu/Main Menu.cs
@@ -4,10 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField]
-    private SceneController _sceneController;
+
     public void play() =>
-        _sceneController.LoadScene("Level 0");
+        SceneManager.LoadScene("Level 0");
 
     public void Exit() =>
         Application.Quit();
