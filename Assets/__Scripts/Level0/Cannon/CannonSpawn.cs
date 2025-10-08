f19f72ad94b84803c73c2c741658cfae36060429
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-10-07 23:38:55 -0500

    changes to cannon audio, removed old cannon prehab
    also removed old non animated cannon prehab
    seperate prehab for cannon spawn and cannon audio

--- Assets/__Scripts/Level0/Cannon/CannonSpawn.cs
+++ Assets/__Scripts/Level0/Cannon/CannonSpawn.cs
@@ -5,44 +5,16 @@ public class CannonSpawn : MonoBehaviour
 {
     public GameObject cannon;
     private float timer;
-    private float timerForSound;
-
-    public GameObject CannonSingleSound;
-
-    public GameObject CannonTrippleShot;
-
-    // Range for random spawn intervals
-    float minSpawnTime = 1f;
-    private float maxSpawnTime = 2f;
-
-    private float nextSpawnTime;
-
-    void Start()
-    {
-        nextSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
-    }
 
     void Update()
     {
+        
         timer += Time.deltaTime;
 
         if (timer >= 1)
         {
             SpawnCannonBall();
             timer = 0f;
-            if (!CannonTrippleShot)
-            {
-                Instantiate(CannonSingleSound, transform.position, Quaternion.identity);
-                return;
-
-            }
-
-            Instantiate(CannonTrippleShot, transform.position, Quaternion.identity);
-        }
-        if(timerForSound > 2)
-        {
-            //StartCoroutine(SpawnCannonShotSound());
-            timerForSound = 0f;
         }
     }
 
@@ -53,4 +25,3 @@ public class CannonSpawn : MonoBehaviour
 
 
 }
-//every 2 seconds -> 3 cannonshot sounds
