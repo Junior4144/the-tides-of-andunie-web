f55471a488bbb1cde628673bca17b34ce8a83fec
dsims2547 <simsdarryl80@gmail.com>
2025-10-18 14:13:01 -0500

    adds typed dialogue and modifies DialogueController.cs

--- Assets/___Scripts/NPC/ITalkable.cs
+++ Assets/___Scripts/NPC/ITalkable.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public interface ITalkable
 {
-    public void Talk(DialogueText dialogueText);
+    //public void Talk(DialogueText dialogueText);
+    public void Talk(string text, float duration, float fontSize);
 }
--- Assets/___Scripts/Units/DialogueNPC.cs
+++ Assets/___Scripts/Units/DialogueNPC.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class DialogueNPC : NPC, ITalkable
 {
-    [SerializeField] private DialogueText dialogueTextScript;
+    // For dialogue bubble
+    private const float _defaultDuration = 3f;
+    [TextArea(2, 3)]
+    [SerializeField] private string _text;
+    [SerializeField] private float _duration = _defaultDuration;
+    [SerializeField] private float _fontSize = 0f;
+    [SerializeField] private DialogueBubbleController _dialogueBubbleController;
+
+    // For dialogue box
+    //[SerializeField] private DialogueText dialogueText;
+    //[SerializeField] private DialogueBoxController dialogueBoxController;
 
     public override void Interact()
     {
-        Talk(dialogueTextScript);
+        //Talk(dialogueText);
+        Talk(_text, _duration, _fontSize);
     }
 
-    public void Talk(DialogueText dialogueText)
+    /*public void Talk(DialogueText dialogueText)
     {
 
+    }*/
+
+    public void Talk(string text, float duration, float fontSize)
+    {
+        _dialogueBubbleController.ShowBubble(text, duration, fontSize);
     }
 }
