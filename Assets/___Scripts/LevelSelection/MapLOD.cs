9665862d377f275139afb4d2c80e80c53b44b1c7
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-01 15:13:57 -0500

    improves Level Selector map design

--- Assets/___Scripts/LevelSelection/MapLOD.cs
+++ Assets/___Scripts/LevelSelection/MapLOD.cs
@@ -16,11 +16,26 @@ public class MapLOD : MonoBehaviour
     public Texture2D lod1;             // 100-200 8k
     public Texture2D lod2;             // 200-300 4k
 
+    public string oceanSortingLayer = "Default";
+    public int oceanSortingOrder = 0;
+
+    public string quadSortingLayer = "Default";
+    public int quadSortingOrder = 1;
 
 
     private void Start()
     {
         cam = CameraManager.Instance.GetCamera();
+
+        quadRenderer.sortingLayerName = quadSortingLayer;
+        quadRenderer.sortingOrder = quadSortingOrder;
+
+        var oceanRenderer = Quad4Oceans.GetComponent<Renderer>();
+        if (oceanRenderer != null)
+        {
+            oceanRenderer.sortingLayerName = oceanSortingLayer;
+            oceanRenderer.sortingOrder = oceanSortingOrder;
+        }
     }
 
     void Update()
@@ -33,21 +48,22 @@ public class MapLOD : MonoBehaviour
             Quad4Oceans.SetActive(true);
 
             tilemap.SetActive(true);
-            quadRenderer.gameObject.SetActive(false);
+            //quadRenderer.gameObject.SetActive(false);
         }
         else if (zoom <= t2)
         {
-            Quad4Oceans.SetActive(false);
+            //Quad4Oceans.SetActive(false);
             tilemap.SetActive(false);
             // first RT
-
+            Quad4Oceans.SetActive(true);
             quadRenderer.gameObject.SetActive(true);
             quadRenderer.material.mainTexture = lod1;
         }
         else if (zoom <= t3)
         {
-            Quad4Oceans.SetActive(false);
-
+            Quad4Oceans.SetActive(true);
+            //Quad4Oceans.SetActive(false);
+            tilemap.SetActive(false);
             quadRenderer.gameObject.SetActive(true);
             quadRenderer.material.mainTexture = lod2;
         }
