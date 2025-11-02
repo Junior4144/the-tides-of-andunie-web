6d1e4c4469e6629c1240e6cc0c9984fe74df4043
ukaday <hayhaygrogh@gmail.com>
2025-11-02 17:45:31 -0600

    adds DebugFormationSaver

--- Assets/___Scripts/Debug/DebugFormationSaver.cs
+++ Assets/___Scripts/Debug/DebugFormationSaver.cs
@@ -1,16 +1,67 @@
+using System.Linq;
 using UnityEngine;
 
 public class DebugFormationSaver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private bool saveOnStart = true;
+    [SerializeField] private KeyCode saveKey = KeyCode.M;
+
     void Start()
     {
-        
+        if (saveOnStart) TrySaveFormation();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(saveKey)) TrySaveFormation();
+    } 
+
+    private void TrySaveFormation()
+    {
+        if (!HasValidManager()) return;
+
+        ClearAndSaveFormation();
+        Destroy(gameObject);
+    }
+
+    private bool HasValidManager()
+    {
+        if (FormationSquadManager.Instance != null) return true;
+
+        Debug.LogError("[DebugFormationSaver] FormationSquadManager.Instance is null. Cannot save formation.");
+        return false;
+    }
+
+    private void ClearAndSaveFormation()
+    {
+        FormationSquadManager.Instance.ClearFormation();
+        SaveChildrenToFormation();
+        LogFormationSaved();
+    }
+
+    private void SaveChildrenToFormation()
+    {
+        Vector2 parentPos = transform.position;
+
+        transform.Cast<Transform>()
+            .ToList()
+            .ForEach(child => AddChildToFormation(child, parentPos));
+    }
+
+    private void AddChildToFormation(Transform child, Vector2 parentPos)
+    {
+        Vector2 offset = (Vector2)child.position - parentPos;
+        string prefabName = child.gameObject.name;
+
+        Debug.Log($"[DebugFormationSaver] Unit saved {prefabName}");
+        Debug.Log($"[DebugFormationSaver] Position {child.position}");
+        Debug.Log($"[DebugFormationSaver] Offset {offset}");
+
+        FormationSquadManager.Instance.AddUnitToFormation(child.gameObject, offset);
+    }
+
+    private void LogFormationSaved()
+    {
+        Debug.Log($"[DebugFormationSaver] Total units {transform.childCount}");
     }
 }
