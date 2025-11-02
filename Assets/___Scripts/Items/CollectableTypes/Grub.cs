94efcc6e2e4aee9ee18783d10989a9e5648d28db
ukaday <hayhaygrogh@gmail.com>
2025-11-01 19:16:29 -0500

    adds coin sound

--- Assets/___Scripts/Items/CollectableTypes/Grub.cs
+++ Assets/___Scripts/Items/CollectableTypes/Grub.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-public class Grub : MonoBehaviour, ICollectable
+public class Grub : Collectable
 {
     [SerializeField] private int _amountOfHealth;
-    [SerializeField] private AudioClip _pickupSound;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,14 +16,5 @@ public class Grub : MonoBehaviour, ICollectable
 
         PlayPickupSound();
         PlayerManager.Instance.AddHealth(_amountOfHealth);
-        Destroy(gameObject);
-    }
-
-    private void PlayPickupSound()
-    {
-        if (_pickupSound != null)
-            AudioSource.PlayClipAtPoint(_pickupSound, transform.position);
-        else
-            Debug.LogWarning("[Grub] PickupSound is null. Playing no sound");
     }
 }
