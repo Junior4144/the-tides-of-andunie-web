09b4a7dc1911a6d3297515649fc93d15a2a2e492
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-02 00:10:20 -0500

    adds new layout for inventory

--- Assets/___Scripts/UI/Slot.cs
+++ Assets/___Scripts/UI/Slot.cs
@@ -6,6 +6,7 @@ public class Slot : MonoBehaviour
 
     public void EquipItem()
     {
+        if (currentItem == null) return;
         InventoryManager.Instance.EquipItem(currentItem.ItemId);
     }
    
--- Assets/___Scripts/UI/UnequipSlot.cs
+++ Assets/___Scripts/UI/UnequipSlot.cs
@@ -7,6 +7,7 @@ public class UnequipSlot : MonoBehaviour
 
     public void HandlesUnequip()
     {
+        if (currentItem == null) return;
         InventoryManager.Instance.UnequipItem(currentItem?.ItemId);
     }
    
