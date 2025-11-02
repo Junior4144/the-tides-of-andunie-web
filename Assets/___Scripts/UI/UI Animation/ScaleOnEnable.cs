c46664c6083f0f5e7c9973a3187eb269edd64ece
ukaday <hayhaygrogh@gmail.com>
2025-11-02 00:20:01 -0500

    removes unused function

--- Assets/___Scripts/UI/UI Animation/ScaleOnEnable.cs
+++ Assets/___Scripts/UI/UI Animation/ScaleOnEnable.cs
@@ -9,10 +9,4 @@ public class ScaleOnEnable : MonoBehaviour
         rt.localScale = Vector3.zero;
         rt.DOScale(1f, 0.35f).SetEase(Ease.OutBack);
     }
-
-    void OnDisable()
-    {
-        RectTransform rt = GetComponent<RectTransform>();
-        rt.DOScale(0f, 0.35f).SetEase(Ease.InBack);
-    }
 }
