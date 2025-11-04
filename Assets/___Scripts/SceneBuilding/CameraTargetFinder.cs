c7ccbca80f222cd990f9425627e10192c471307e
ukaday <hayhaygrogh@gmail.com>
2025-11-03 22:48:37 -0600

    fix CameraTargetFinder null issue, lower dialogue volume

--- Assets/___Scripts/SceneBuilding/CameraTargetFinder.cs
+++ Assets/___Scripts/SceneBuilding/CameraTargetFinder.cs
@@ -5,8 +5,20 @@ public class CameraTargetFinder : MonoBehaviour
 {
     void Start()
     {
-        var cam = GetComponent<CinemachineCamera>();
-        if (PlayerManager.Instance?.gameObject.transform) cam.Follow = PlayerManager.Instance.gameObject.transform;
+        var camera = GetComponent<CinemachineCamera>();
+        if (camera == null) return;
 
+        var playerTransform = GetPlayerTransform();
+        if (playerTransform == null) return;
+
+        camera.Follow = playerTransform;
+    }
+
+    private Transform GetPlayerTransform()
+    {
+        if (PlayerManager.Instance == null) return null;
+        if (PlayerManager.Instance.gameObject == null) return null;
+
+        return PlayerManager.Instance.gameObject.transform;
     }
 }
