2f600494c108e4447888e2fa9994ad880a100c13
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-03 20:45:52 -0600

    fixes village 2 flag logic in Level Selector

--- Assets/___Scripts/LevelSelection/WorldSpaceCanvasCameraLink.cs
+++ Assets/___Scripts/LevelSelection/WorldSpaceCanvasCameraLink.cs
@@ -5,27 +5,13 @@ public class WorldSpaceCanvasCameraLink : MonoBehaviour
 {
     private Canvas _canvas;
 
-    private void Awake()
-    {
-        _canvas = GetComponent<Canvas>();
-    }
+    private void Awake() => _canvas = GetComponent<Canvas>();
 
     private void Start()
     {
-        // Try to grab camera from CameraManager
         if (CameraManager.Instance != null && CameraManager.Instance.GetCamera() != null)
-        {
             _canvas.worldCamera = CameraManager.Instance.GetCamera();
-            Debug.Log($"[WorldSpaceCanvasCameraLink] Assigned camera '{_canvas.worldCamera.name}' to Canvas '{gameObject.name}'.");
-        }
         else
-        {
-            // Fallback: try main camera
             _canvas.worldCamera = Camera.main;
-            if (_canvas.worldCamera != null)
-                Debug.Log($"[WorldSpaceCanvasCameraLink] Used Camera.main for '{gameObject.name}'.");
-            else
-                Debug.LogWarning($"[WorldSpaceCanvasCameraLink] No camera found for Canvas '{gameObject.name}'!");
-        }
     }
 }
