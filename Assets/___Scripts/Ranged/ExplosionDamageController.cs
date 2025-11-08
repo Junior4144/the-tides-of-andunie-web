5c607653e1ab501eb3a03f504532afbbdb4a0360
ukaday <hayhaygrogh@gmail.com>
2025-11-07 18:33:19 -0600

    fixes explosion hitting multiple times

--- Assets/___Scripts/Ranged/ExplosionDamageController.cs
+++ Assets/___Scripts/Ranged/ExplosionDamageController.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 
 public class ExplosionDamageController : MonoBehaviour
 {
-    private readonly HashSet<Collider2D> hitEnemies = new();
+    private readonly HashSet<GameObject> hitEnemies = new();
 
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (hitEnemies.Contains(collision)) return;
+        GameObject target = collision.gameObject;
+
+        if (hitEnemies.Contains(target))
+        {
+            Debug.Log($"[ExplosionDamageController] Already hit {target.name}");
+            return;
+        }
 
         if (collision.TryGetComponent(out IHealthController health))
         {
-            hitEnemies.Add(collision);
+            hitEnemies.Add(target);
+            Debug.Log($"[ExplosionDamageController] Damage dealt {PlayerStatsManager.Instance.DefaultExplosionDamage}");
             health.TakeDamage(PlayerStatsManager.Instance.DefaultExplosionDamage);
         }
     }
