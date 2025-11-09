faf8604c9f22348c03b5958b3c98fdb48443bd4c
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-08 23:36:12 -0600

    Adds weapon selector logic to weapon HUD

--- Assets/___Scripts/UI/WeaponHUD/WeaponHUDController.cs
+++ Assets/___Scripts/UI/WeaponHUD/WeaponHUDController.cs
@@ -1,23 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponHUDController : MonoBehaviour
 {
+    [SerializeField] private Image AxeWeaponIcon;
+    [SerializeField] private Image BowWeaponIcon;
+
     [SerializeField] private GameObject AxeCoolDown;
     [SerializeField] private GameObject BowCoolDown;
 
     private void OnEnable()
     {
+        WeaponEvents.OnNewWeaponEquipped += HandleEquippedWeapon;
         WeaponEvents.OnWeaponAbilityActivation += HandleAbilityRequest;
     }
 
     private void OnDisable()
     {
+        WeaponEvents.OnNewWeaponEquipped -= HandleEquippedWeapon;
         WeaponEvents.OnWeaponAbilityActivation -= HandleAbilityRequest;
     }
     private void Start()
     {
         AxeCoolDown.SetActive(false);
         BowCoolDown.SetActive(false);
+    }
+
+    private void HandleEquippedWeapon(WeaponType equippedWeapon)
+    {
+        SetWeaponTransparency(AxeWeaponIcon, 0.3f);
+        SetWeaponTransparency(BowWeaponIcon, 0.3f);
+
+        switch (equippedWeapon)
+        {
+            case WeaponType.Axe:
+                SetWeaponTransparency(AxeWeaponIcon, 1f);
+                break;
+
+            case WeaponType.Bow:
+                SetWeaponTransparency(BowWeaponIcon, 1f);
+                break;
+        }
+    }
+
+    private void SetWeaponTransparency(Image icon, float alpha)
+    {
+        Color color = icon.color;
+        color.a = alpha;
+        icon.color = color;
     }
 
     private void HandleAbilityRequest(WeaponType weaponType)
