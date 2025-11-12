3492f545093f1f5ccfdb5d9646d0ff658e833e3c
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-12 15:14:03 -0600

    adds archer pirate

--- Assets/___Scripts/Ranged/MusketBallProjectile.cs
+++ Assets/___Scripts/Ranged/MusketBallProjectile.cs
@@ -25,7 +25,7 @@ public class MusketBallProjectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        Debug.Log($"Projectile hit {collision.name}");
         if (collision.TryGetComponent(out IHealthController health))
         {
             if (hasDamage) return;
