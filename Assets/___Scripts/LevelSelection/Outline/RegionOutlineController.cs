911566b375e36d2325597314a1672530aaa01e8c
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-17 15:41:54 -0600

    Update region controllers function naming

--- Assets/___Scripts/LevelSelection/Outline/RegionOutlineController.cs
+++ Assets/___Scripts/LevelSelection/Outline/RegionOutlineController.cs
@@ -13,23 +13,23 @@ public class RegionOutlineController : MonoBehaviour
 
     private void OnEnable()
     {
-        RegionZoomController.ZoomAboveThreshold += OnZoomAbove100;
-        RegionZoomController.ZoomBelowThreshold += OnZoomBelow100;
+        RegionZoomController.ZoomAboveThreshold += ZoomAboveThreshold;
+        RegionZoomController.ZoomBelowThreshold += ZoomBelowThreshold;
     }
 
     private void OnDisable()
     {
-        RegionZoomController.ZoomAboveThreshold -= OnZoomAbove100;
-        RegionZoomController.ZoomBelowThreshold -= OnZoomBelow100;
+        RegionZoomController.ZoomAboveThreshold -= ZoomAboveThreshold;
+        RegionZoomController.ZoomBelowThreshold -= ZoomBelowThreshold;
     }
 
-    private void OnZoomBelow100()
+    private void ZoomBelowThreshold()
     {
         Outline.enabled = false;
         onClickOutline.enabled = false;
     }
 
-    private void OnZoomAbove100()
+    private void ZoomAboveThreshold()
     {
         Outline.enabled = true;
         onClickOutline.enabled = true;
