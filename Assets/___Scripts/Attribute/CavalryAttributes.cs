08391aa5f32e56c766beee50c34764bfcf1f02c8
Mohamed Salah <mosalah3480@gmail.com>
2025-11-18 15:43:03 -0600

    Create Cavalry AI states - Charge and patrol
    - Create charge and patrol logic in the movement controller script
    - Fine-tune attributes and set the default values in the scriptable object
    - Create the Cavalry prefab
    
    ref #102

--- Assets/___Scripts/Attribute/CavalryAttributes.cs
+++ Assets/___Scripts/Attribute/CavalryAttributes.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Cavalry Attributes", menuName = "Scriptable Objects/Cavalry Attributes")]
 public class CavalryAttributes : ScriptableObject
 {
-    public float RotationSpeed;
+    [Tooltip("How fast the cavalry can rotate, in degrees per second.")]
+    [Range(10f, 360f)]
+    public float TurningSpeed = 120f;
 
-    public float Health;
+    public float Health = 300f;
 
-    public float DamageAmount;
+    public float DamageAmount = 50f;
 
     public float ReadyDistance;
 
-    public float MinSpeedForTurning;
+    public float MinSpeedForTurning = 10f;
 
-    public float MaxSpeedForTurning;
+    public float MaxSpeedForTurning = 45f;
 
-    public float ChargeSpeed;
-    public float TurnSpeed;
-    public float ChargeAngle;
+    public float ChargeSpeed = 50f;
+    public float PatrollingSpeed = 10f;
+    public float ChargeAngle = 20f;
 
-    public float Acceleration = 5f;
+    public float Acceleration = 10f;
+    public float ChargeAcceleration = 25f;
+    public float Deceleration = 120f;
 
+    public float MinPathStraightnessToAttack = 0.9f;
+
+
+    public float AttackCoolDown = 5f;
     
 
     [field: Range(0, 1)]
--- Assets/___Scripts/Movement/Cavalry/CavalryMovementController.cs
+++ Assets/___Scripts/Movement/Cavalry/CavalryMovementController.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
+using System.Collections.Generic;
+using Unity.Properties;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(Rigidbody2D))]
@@ -11,6 +14,16 @@ public class CavalryMovementController : MonoBehaviour
     private NavMeshAgent agent;
     private Rigidbody2D _rigidbody;
 
+    private enum CavalryState { Patrolling, Attacking }
+    private CavalryState _currentState;
+    private bool _hasHitThePlayer;
+
+    [SerializeField] private List<Transform> PatrolPointsSequence;
+    private int _currentPatrolPointIndex;
+    private NavMeshPath _pathPlaceholder;
+
+    private float _lastAttackTime;
+
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -18,8 +31,11 @@ public class CavalryMovementController : MonoBehaviour
         agent.updatePosition = false;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-
-        agent.speed = _attributes.TurnSpeed;
+        _pathPlaceholder = new NavMeshPath();
+        _currentPatrolPointIndex = 0;
+        TransitionToPatrollingState();
+        _hasHitThePlayer = false;
+        _lastAttackTime = -_attributes.AttackCoolDown;
     }
 
     private Transform Player => _player == null ? _player = PlayerManager.Instance.transform : _player;
@@ -38,32 +54,155 @@ public class CavalryMovementController : MonoBehaviour
 
     void Update()
     {
+        
         if (!agent.enabled || Player == null) return;
+
+        switch (_currentState)
+        {
+            case CavalryState.Patrolling:
+                bool canAttack = Time.time - _lastAttackTime > _attributes.AttackCoolDown;
+                if (canAttack && IsLinedUpWithTarget(Player.transform.position) && IsStraightPathToPlayer())
+                    TransitionToAttackingState();
+                else
+                    Patrol();
+                break;
+
+            case CavalryState.Attacking:
+                if (!IsLinedUpWithTarget(Player.transform.position) || !IsStraightPathToPlayer() || _hasHitThePlayer)
+                {
+                    SetNearestPatrolPoint();
+                    TransitionToPatrollingState();
+                }
+                else 
+                    ChasePlayer();
+                break;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_currentState == CavalryState.Attacking && collision.gameObject.CompareTag("Player"))
+            HandlePlayerHit();
+        
+    }
+
+    private void HandlePlayerHit()
+    {
+        _hasHitThePlayer = true;
+        _lastAttackTime = Time.time;
+    }
+
+    private void Patrol()
+    {
+        Vector3 CurrentPatrolPointPosition() => PatrolPointsSequence[_currentPatrolPointIndex].position;
+        ChangeAgentSpeedTowards(_attributes.PatrollingSpeed);
+        if (CalculateDisplacementToTarget(CurrentPatrolPointPosition()) <= 1.0f)
+        {
+            _currentPatrolPointIndex = 
+            _currentPatrolPointIndex + 1 == PatrolPointsSequence.Count()?
+            0 : _currentPatrolPointIndex + 1;
+        }
+        agent.SetDestination(CurrentPatrolPointPosition());
+    }
+
+    private void ChasePlayer()
+    {
         agent.SetDestination(Player.position);
-        UpdateTargetSpeed();
-    }
-
-    private void UpdateTargetSpeed()
-    {
-        float targetSpeed = IsLinedUpForCharge() ? _attributes.ChargeSpeed : _attributes.TurnSpeed;
-
-        agent.speed = Mathf.MoveTowards(
-            agent.speed,
-            targetSpeed,
-            _attributes.Acceleration * Time.deltaTime
-        );
-    }
-
-    private bool IsLinedUpForCharge()
-    {
-        Vector3 directionToTarget = (agent.steeringTarget - (Vector3)_rigidbody.position).normalized;
-        Vector3 currentForward = transform.up;
-        float angleToSteer = Vector3.Angle(currentForward, directionToTarget);
-        return angleToSteer <= _attributes.ChargeAngle;
+        UpdateChaseSpeed();
     }
 
     
+    private void TransitionToAttackingState()
+    {
+        Debug.Log("Cavalry Attacking!");
+        _currentState = CavalryState.Attacking;
+    }
 
+    private void TransitionToPatrollingState()
+    {
+        Debug.Log("Cavalry Patrolling...");
+        _currentState = CavalryState.Patrolling;
+        _hasHitThePlayer = false;
+    }
+
+    private float CalculateDistanceToTarget(Vector3 targetPosition)
+    {
+        if (!agent.CalculatePath(targetPosition, _pathPlaceholder) || _pathPlaceholder.status != NavMeshPathStatus.PathComplete)
+        {
+            return Mathf.Infinity;
+        }
+
+        float totalDistance = _pathPlaceholder.corners.Zip(_pathPlaceholder.corners.Skip(1), Vector3.Distance).Sum();
+
+        return totalDistance;
+    }
+
+    private float CalculateDisplacementToTarget(Vector3 targetPosition) => Vector3.Distance(_rigidbody.position, targetPosition);
+
+    private void SetNearestPatrolPoint()
+    {
+        if (PatrolPointsSequence == null || PatrolPointsSequence.Count == 0){
+            Debug.LogError("No PatrolPoints assigned to Cavalry unit.");
+             return;
+        }   
+
+        int nearestIndex = -1;
+        float minDistance = Mathf.Infinity;
+
+        for (int i = 0; i < PatrolPointsSequence.Count; i++)
+        {
+            float distance = CalculateDistanceToTarget(PatrolPointsSequence[i].position);
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestIndex = i;
+            }
+        }
+
+        _currentPatrolPointIndex = nearestIndex;
+    }
+
+    private void UpdateChaseSpeed()
+    {
+        float targetSpeed = IsLinedUpWithTarget(Player.transform.position) ? _attributes.ChargeSpeed : _attributes.PatrollingSpeed;
+        ChangeAgentSpeedTowards(targetSpeed, _attributes.ChargeAcceleration);
+    }
+
+    private void ChangeAgentSpeedTowards(float targetSpeed, float rate = 0.0f){
+        float currentSpeed = agent.speed;
+        
+        if (rate == 0) rate = currentSpeed < targetSpeed ?
+        _attributes.Acceleration : _attributes.Deceleration;
+
+        
+        agent.speed = Mathf.MoveTowards(
+            agent.speed,
+            targetSpeed,
+            rate * Time.deltaTime
+        );
+    }
+
+    private bool IsLinedUpWithTarget(Vector3 targetPosition)
+    {
+        Vector3 directionToTarget = (targetPosition - (Vector3)_rigidbody.position).normalized;
+        Vector3 currentForward = transform.up;
+        float angleToTarget = Vector3.Angle(currentForward, directionToTarget);
+        return angleToTarget <= _attributes.ChargeAngle;
+    }
+
+
+    private bool IsStraightPathToPlayer()
+    {
+        float displacement = CalculateDisplacementToTarget(Player.position);
+        float pathDistance = CalculateDistanceToTarget(Player.position);
+
+        if (pathDistance == Mathf.Infinity || pathDistance < 0.01f)
+            return false;
+
+        float straightness = displacement / pathDistance;
+        return straightness >= _attributes.MinPathStraightnessToAttack;
+    }
     private void ApplySpeedBasedSteering()
     {
         Vector3 steeringDirection = (agent.steeringTarget - (Vector3)_rigidbody.position).normalized;
@@ -76,7 +215,7 @@ public class CavalryMovementController : MonoBehaviour
 
         float turnAbility = 1.0f - Mathf.InverseLerp(_attributes.MinSpeedForTurning, _attributes.MaxSpeedForTurning, currentSpeed);
 
-        float effectiveTurnSpeed = _attributes.RotationSpeed * turnAbility;
+        float effectiveTurnSpeed = _attributes.TurningSpeed * turnAbility;
 
         _rigidbody.SetRotation(
             Mathf.MoveTowardsAngle(
