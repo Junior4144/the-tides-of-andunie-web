0e678c6a19cd52778525c3130d1ea8cb3f47a45b
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-22 01:49:20 -0600

    changes to LS music background setup

--- Assets/___Scripts/LevelSelection/MusicShuffler.cs
+++ Assets/___Scripts/LevelSelection/MusicShuffler.cs
@@ -1,6 +1,6 @@
+using System.Collections;
 using UnityEngine;
-
-
+using UnityEngine.SceneManagement;
 
 [RequireComponent (typeof(AudioSource))]
 public class MusicShuffler : MonoBehaviour
@@ -9,13 +9,33 @@ public class MusicShuffler : MonoBehaviour
     public AudioClip[] clips;
     int i;
 
-    void Start()
+    private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
+    }
 
+    private void OnEnable() => SceneManager.activeSceneChanged += HandleCheck;
+
+    private void OnDisable() => SceneManager.activeSceneChanged -= HandleCheck;
+
+    private void HandleCheck(Scene oldScene, Scene newScene)
+    {
+        StartCoroutine(CheckAfterLoading(newScene));
+    }
+
+    private IEnumerator CheckAfterLoading(Scene newScene)
+    {
+        yield return null;
+
+        if (newScene == gameObject.scene)
+            HandlePlayingMusic();
+    }
+
+    private void HandlePlayingMusic()
+    {
+        _audioSource = GetComponent<AudioSource>();
         PlayNext();
     }
-        
 
     void Update()
     {
