96b9fcec0f53aad3a7272a5080a7bdddfdb97355
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-11-25 13:35:05 -0600

    Updates tooltips to handle pre screen issues

--- Assets/___Scripts/Collectables/ToolTipsUIController.cs
+++ Assets/___Scripts/Collectables/ToolTipsUIController.cs
@@ -11,15 +11,17 @@ public class ToolTipsUIController : MonoBehaviour
         UIEvents.OnShopListingHover += HandleUIActivation;
         UIEvents.OnShopListingExit += HandleUIDeactivation;
 
-
+        UIEvents.OnPreScreenDeactivated += HandleUIDeactivation;
         UIEvents.OnShopDeactivated += HandleUIDeactivation;
         UIEvents.OnPauseMenuActive += HandleUIDeactivation;
     }
+
     private void OnDisable()
     {
         UIEvents.OnShopListingHover -= HandleUIActivation;
         UIEvents.OnShopListingExit -= HandleUIDeactivation;
 
+        UIEvents.OnPreScreenDeactivated -= HandleUIDeactivation;
         UIEvents.OnShopDeactivated -= HandleUIDeactivation;
         UIEvents.OnPauseMenuActive -= HandleUIDeactivation;
     }
