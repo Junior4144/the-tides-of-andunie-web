26951ad335cc8d9b18bfc9a01328afd7d1fd01e6
mosalah_windowsPC <mosalah3480@gmail.com>
2025-11-27 21:33:18 -0600

    Create adjusted copies of health and melee scripts for cavalry

---
+++ Assets/___Scripts/Melee/CavalryMeleeController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+
+public class CavalryMeleeController : MonoBehaviour
+{
+    [SerializeField] private CavalryAttributes _attributes;
+    [SerializeField] private MeleeEnemyAnimator _animator;
+
+    [SerializeField] private float _damageDelay = 0f;
+    [SerializeField] private float _animDuration;
+    [SerializeField] private float _damageRange = 5f;
+
+    private float _lastAttackTime;
+    private bool _isAttacking = false;
+
+    void Awake()
+    {
+        _lastAttackTime = -_attributes.AttackCoolDown;
+    }
+
+    public void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent(typeof(IHealthController)) as IHealthController;
+        if (
+            IsEnemy(otherCollider) &&
+            health != null &&
+            !_isAttacking &&
+            Time.time - _lastAttackTime > _attributes.AttackCoolDown
+        )
+        {
+            Debug.Log("Hitting the player now");
+            _lastAttackTime = Time.time;
+            StartCoroutine(Attack(otherCollider.gameObject));
+            PlayAttackAnimation();
+        }
+    }
+
+    private bool IsEnemy(Collider2D otherCollider) => 
+        otherCollider.gameObject.layer == LayerMask.NameToLayer("Friendly");
+    
+
+    private IEnumerator Attack(GameObject enemyObject)
+    {
+        yield return new WaitForSeconds(_damageDelay);
+
+        if (enemyObject)
+        {
+            float distance = Vector2.Distance(transform.position, enemyObject.transform.position);
+            
+            if (distance <= _damageRange)
+                enemyObject.GetComponent<IHealthController>().TakeDamage(_attributes.DamageAmount);
+        }
+    }
+
+    private void PlayAttackAnimation()
+    {
+        if (_animator)
+        {   
+            _isAttacking = true;
+            _animator.TriggerAttack();
+            StartCoroutine(ResetAttackAnimation());
+        }
+        else
+            Debug.LogWarning("Animator is Null. Playing no Animation");
+    }
+
+    private IEnumerator ResetAttackAnimation()
+    {
+        yield return new WaitForSeconds(_animDuration);
+        _isAttacking = false;
+    }
+}
