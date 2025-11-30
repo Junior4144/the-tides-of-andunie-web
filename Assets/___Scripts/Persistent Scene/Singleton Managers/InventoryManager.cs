963e5f2bdb67d29e331743578a9a2f3e8d34f1fa
ukaday <hayhaygrogh@gmail.com>
2025-11-30 06:21:57 -0600

    adds conditional auto equip on add item

--- Assets/___Scripts/Persistent Scene/Singleton Managers/InventoryManager.cs
+++ Assets/___Scripts/Persistent Scene/Singleton Managers/InventoryManager.cs
@@ -31,16 +31,21 @@ public class InventoryManager : MonoBehaviour
     public bool AddItem(InventoryItem item, int quantity = 1)
     {
         if (!IsValidAddition(item, quantity)) return false;
+
+        bool result;
+
         if (HasItem(item.ItemId))
         {
-            bool result =  AddToExistingSlot(item.ItemId, quantity);
-            OnInventoryChanged?.Invoke();
-            return result;
-        }
+            result = AddToExistingSlot(item.ItemId, quantity);
 
-        bool res = CreateNewSlot(item, quantity);
+            if (result && HasItemEquipped(item.ItemId))
+                EquipItem(item.ItemId, quantity);
+        }
+        else
+            result = CreateNewSlot(item, quantity);
+
         OnInventoryChanged?.Invoke();
-        return res;
+        return result;
     }
 
     public bool RemoveItem(string itemId, int quantity = 1)
