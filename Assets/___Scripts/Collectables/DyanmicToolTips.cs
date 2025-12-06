a87a895797291814bb8d8406e4526a5f537ff9dc
Gustavo Buenrostro <94208651+Junior4144@users.noreply.github.com>
2025-12-06 02:22:49 -0600

    removal of packages, and some audio optimization

--- Assets/___Scripts/Collectables/DyanmicToolTips.cs
+++ Assets/___Scripts/Collectables/DyanmicToolTips.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using TMPro;
-using Unity.AppUI.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Assets/___Scripts/LevelSelection/Outline/RegionUIController.cs
+++ Assets/___Scripts/LevelSelection/Outline/RegionUIController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.AppUI.UI;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.Rendering.DebugUI;
